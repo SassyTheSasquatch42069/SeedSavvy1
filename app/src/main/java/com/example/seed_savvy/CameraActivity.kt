274a0a9474d6c9192adee11bc0d050f4c043143b
Tauriq <Samuelstauriq@gmail.com>
2024-05-16 23:27:05 +0200

    CameraActivity.kt and fixed up manifest.xml file

---
+++ app/src/main/java/com/example/seed_savvy/CameraActivity.kt
@@ -0,0 +1,20 @@
+package com.example.seed_savvy
+import androidx.appcompat.app.AppCompatActivity
+
+//import android.Manifest
+//import android.content.pm.PackageManager
+//import android.graphics.SurfaceTexture
+//import android.hardware.camera2.*
+//import android.os.Bundle
+//import android.os.Handler
+//import android.os.HandlerThread
+//import android.util.Log
+//import android.view.Surface
+//import android.view.TextureView
+//import androidx.appcompat.app.AppCompatActivity
+//import androidx.core.app.ActivityCompat
+//import androidx.core.content.ContextCompat
+
+class CamaraActivity : AppCompatActivity() {
+
+}
